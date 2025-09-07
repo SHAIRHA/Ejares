7ab930f3efad4b4c2194a2810dc69dd5848f03ca
SHAIRHA <shairhamay.ejares@dbtc-cebu.edu.ph>
2025-09-07 19:32:57 +0800

    Initial commit

---
+++ Student_Record_Project/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+
+class NodeStudent
+{
+    public int ID;
+    public string Name;
+    public string Course;
+    public int YearLevel;
+    public double GPA;
+    public NodeStudent Next;
+
+    public NodeStudent(int id, string name, string course, int yearLevel, double gpa)
+    {
+        ID = id;
+        Name = name;
+        Course = course;
+        YearLevel = yearLevel;
+        GPA = gpa;
+        Next = null;
+    }
+}
+
+class StudentList
+{
+    private NodeStudent head;
+
+    public void AddStudentAtBeginning(NodeStudent newStudent)
+    {
+        newStudent.Next = head;
+        head = newStudent;
+        Console.WriteLine("Student added at the beginning of the list.\n");
+        DisplayStudentDetails("BEGINNING", newStudent);
+    }
+
+    public void AddStudentAtEnd(NodeStudent newStudent)
+    {
+        if (head == null)
+        {
+            head = newStudent;
+            Console.WriteLine("Student added at the beginning of the list.\n");
+            DisplayStudentDetails("BEGINNING", newStudent);
+        }
+        else
+        {
+            NodeStudent temp = head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+            temp.Next = newStudent;
+            Console.WriteLine("Student added at the end of the list.\n");
+            DisplayStudentDetails("END", newStudent);
+        }
+    }
+
+    public void AddStudentAtPosition(NodeStudent newStudent, int position)
+    {
+        if (position <= 1 || head == null)
+        {
+            AddStudentAtBeginning(newStudent);
+            return;
+        }
+
+        NodeStudent temp = head;
+        int count = 1;
+
+        while (temp != null && count < position - 1)
+        {
+            temp = temp.Next;
+            count++;
+        }
+
+        if (temp == null)
+        {
+            Console.WriteLine("Invalid Position. Try Again!.\n");
+            AddStudentAtEnd(newStudent);
+        }
+        else
+        {
+            newStudent.Next = temp.Next;
+            temp.Next = newStudent;
+            Console.WriteLine("Student added at position " + position + ".\n");
+            DisplayStudentDetails("POSITION " + position, newStudent);
+        }
+    }
+
+    private void DisplayStudentDetails(string location, NodeStudent student)
+    {
+        Console.WriteLine($"------------------------------------------------------------------------------------------------------------");
+        Console.WriteLine($"                                       Student Record:                                                      ");
+        Console.WriteLine($" {location}                                                                                                 ");
+        Console.WriteLine($" ID={student.ID}, Name={student.Name}, Course={student.Course}, Year={student.YearLevel}, GPA={student.GPA} ");
+        Console.WriteLine($"------------------------------------------------------------------------------------------------------------");
+    }
+
+    public void DeleteStudent(int id)
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Student List is empty.\n");
+            return;
+        }
+
+        if (head.ID == id)
+        {
+            head = head.Next;
+            Console.WriteLine("Student deleted.\n");
+            return;
+        }
+
+        NodeStudent temp = head;
+        while (temp.Next != null && temp.Next.ID != id)
+        {
+            temp = temp.Next;
+        }
+
+        if (temp.Next == null)
+        {
+            Console.WriteLine("Student not found. Try Again!\n");
+        }
+        else
+        {
+            temp.Next = temp.Next.Next;
+            Console.WriteLine("Student deleted.\n");
+        }
+    }
+
+    public void SearchStudent(int id)
+    {
+        NodeStudent temp = head;
+        while (temp != null)
+        {
+            if (temp.ID == id)
+            {
+                Console.WriteLine($"Found: ID={temp.ID}, Name={temp.Name}, Course={temp.Course}, Year={temp.YearLevel}, GPA={temp.GPA}\n");
+                return;
+            }
+            temp = temp.Next;
+        }
+        Console.WriteLine("Student not found. Try Again!\n");
+    }
+
+    public void UpdateStudent(int id)
+    {
+        NodeStudent temp = head;
+        while (temp != null)
+        {
+            if (temp.ID == id)
+            {
+                Console.Write("Enter new Name: ");
+                temp.Name = Console.ReadLine();
+                Console.Write("Enter new Course: ");
+                temp.Course = Console.ReadLine();
+                Console.Write("Enter new Year Level: ");
+                temp.YearLevel = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter new GPA: ");
+                temp.GPA = (float)Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Student record updated.\n");
+                return;
+            }
+            temp = temp.Next;
+        }
+        Console.WriteLine("Student not found. Try Again!\n");
+    }
+
+    public void DisplayAllStudents()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("No student records found.\n");
+            return;
+        }
+
+        NodeStudent temp = head;
+        Console.WriteLine("All Student Records:");
+        while (temp != null)
+        {
+            Console.WriteLine($"ID={temp.ID}, Name={temp.Name}, Course={temp.Course}, Year={temp.YearLevel}, GPA={temp.GPA}");
+            temp = temp.Next;
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ Student_Record_Project/Student_Record.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Record_Project
+{
+    public class Program
+    {
+        static void Main()
+        {
+            StudentList list = new StudentList();
+            bool running = true;
+            int choice;
+
+            while (running)
+            {
+                Console.WriteLine("-------------------------------------------");
+                Console.WriteLine("|     Student Record Management System    |");
+                Console.WriteLine("| 1. Add Student                          |");
+                Console.WriteLine("| 2. Delete Student                       |");
+                Console.WriteLine("| 3. Search Student                       |");
+                Console.WriteLine("| 4. Update Student                       |");
+                Console.WriteLine("| 5. Display All Students                 |");
+                Console.WriteLine("| 6. Exit                                 |");
+                Console.WriteLine("-------------------------------------------");
+                Console.Write("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                if (choice == 1)
+                {
+                    Console.Write("Enter ID: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter Course: ");
+                    string course = Console.ReadLine();
+                    Console.Write("Enter Year Level: ");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter GPA: ");
+                    double gpa = Convert.ToDouble(Console.ReadLine());
+
+                    NodeStudent newStudent = new NodeStudent(id, name, course, year, gpa);
+
+                    Console.WriteLine("-------------------------------------------");
+                    Console.WriteLine("|     Insert at:                          |");
+                    Console.WriteLine("| 1. Beginning                            |");
+                    Console.WriteLine("| 2. End                                  |");
+                    Console.WriteLine("| 3. Specific Location                    |");
+                    Console.WriteLine("-------------------------------------------");
+                    Console.Write("Enter your choice: ");
+                    int insertChoice = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+
+                    if (insertChoice == 1)
+                        list.AddStudentAtBeginning(newStudent);
+                    else if (insertChoice == 2)
+                        list.AddStudentAtEnd(newStudent);
+                    else if (insertChoice == 3)
+                    {
+                        Console.Write("Enter position: ");
+                        int position = Convert.ToInt32(Console.ReadLine());
+                        list.AddStudentAtPosition(newStudent, position);
+                    }
+                    else
+                        Console.WriteLine("Invalid choice. Try Again\n");
+                }
+                else if (choice == 2)
+                {
+                    Console.Write("Enter ID to delete: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    list.DeleteStudent(id);
+                }
+                else if (choice == 3)
+                {
+                    Console.Write("Enter ID to search: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    list.SearchStudent(id);
+                }
+                else if (choice == 4)
+                {
+                    Console.Write("Enter ID to update: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    list.UpdateStudent(id);
+                }
+                else if (choice == 5)
+                {
+                    list.DisplayAllStudents();
+                }
+                else if (choice == 6)
+                {
+                    Console.WriteLine("Exiting...");
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Try Again.\n");
+                }
+
+            }
+        }
+    }
+
+}
